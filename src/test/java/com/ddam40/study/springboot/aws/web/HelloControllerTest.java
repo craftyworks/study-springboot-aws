2cbebfa71a4972f10a5b2e304835403ed47611a8
ddam40 <craftyworks@gmail.com>
2020-01-24 20:25:25 +0900

    수정중

--- src/test/java/com/ddam40/study/springboot/aws/web/HelloControllerTest.java
+++ src/test/java/com/ddam40/study/springboot/aws/web/HelloControllerTest.java
@@ -1,15 +1,15 @@
 package com.ddam40.study.springboot.aws.web;
 
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HelloController.class)
